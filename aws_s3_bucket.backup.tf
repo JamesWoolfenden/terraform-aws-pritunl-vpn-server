a93a2ffe3f006e9ae378c04327c7b2d5f7dc978d
James Woolfenden <James.Woolfenden@gmail.com>
2021-04-15 08:16:11 +0100

    merge failures fixed

--- aws_s3_bucket.backup.tf
+++ aws_s3_bucket.backup.tf
@@ -3,7 +3,7 @@
 resource "aws_s3_bucket" "backup" {
   # tfsec:ignore:AWS002
   # tfsec:ignore:AWS077
-  # checkov:skip=CKV_AWS_144: ADD REASON
+  # checkov:skip=CKV_AWS_144: not appropriate
   # checkov:skip=CKV_AWS_18: "Ensure the S3 bucket has access logging enabled"
   # checkov:skip=CKV_AWS_52: "Ensure S3 bucket has MFA delete enabled"
   # checkov:skip=CKV_AWS_21: "Ensure all data stored in the S3 bucket have versioning enabled"
