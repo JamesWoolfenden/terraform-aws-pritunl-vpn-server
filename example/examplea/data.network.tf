e6b3316cf15de89eab7aefd9b938109d186f0d1e
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 20:12:38 +0100

    fix: replace deprecated aws_subnet_ids with aws_subnets

--- example/examplea/data.network.tf
+++ example/examplea/data.network.tf
@@ -10,26 +10,32 @@ locals {
 data "aws_vpc" "vpc" {
   id = local.vpc_id
 }
-data "aws_subnet_ids" "private" {
-  vpc_id = local.vpc_id
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
 
   #This should not be public -testing only
   tags = {
     Type = "Public"
   }
 }
-data "aws_subnet_ids" "public" {
-  vpc_id = local.vpc_id
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
 
   tags = {
     Type = "Public"
   }
 }
 data "aws_subnet" "public" {
-  count = length(tolist(data.aws_subnet_ids.public.ids))
-  id    = tolist(data.aws_subnet_ids.public.ids)[count.index]
+  count = length(tolist(data.aws_subnets.public.ids))
+  id    = tolist(data.aws_subnets.public.ids)[count.index]
 }
 data "aws_subnet" "private" {
-  count = length(tolist(data.aws_subnet_ids.private.ids))
-  id    = tolist(data.aws_subnet_ids.private.ids)[count.index]
+  count = length(tolist(data.aws_subnets.private.ids))
+  id    = tolist(data.aws_subnets.private.ids)[count.index]
 }
