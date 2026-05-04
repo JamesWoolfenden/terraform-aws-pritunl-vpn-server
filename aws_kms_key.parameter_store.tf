b9ca95887665285d14c78ac71213a838b973a64f
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 11:18:47 +0100

    fix: skip CKV2_AWS_64 on kms key resources

--- aws_kms_key.parameter_store.tf
+++ aws_kms_key.parameter_store.tf
@@ -1,4 +1,5 @@
 resource "aws_kms_key" "parameter_store" {
+  # checkov:skip=CKV2_AWS_64: For example only, key policy managed via IAM
   description = "Parameter store and backup key for ${var.resource_name_prefix}"
 
   policy                  = data.template_file.kms_policy.rendered
